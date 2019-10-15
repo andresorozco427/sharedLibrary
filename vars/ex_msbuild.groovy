bc706144cecdf3fa137f2be8d2d38bbb6005f268
andres orozco muñoz <andresorozco427@gmail.com>
2019-10-15 15:54:40 -0500

    Construccion base de datos

--- vars/ex_msbuild.groovy
+++ vars/ex_msbuild.groovy
@@ -2,13 +2,13 @@ import org.somecompany.build.MsBuild
 import org.somecompany.ioc.ContextRegistry
 
 def call(String solutionPath){
-    ContextRegistry.registerDEfaultContext(solutionPath)
+    ContextRegistry.registerDEfaultContext(this)
     def msbuild = new MsBuild()
     msbuild.build(solutionPath)
 }
 
 def buildDatabase(String serverName){
-    ContextRegistry.registerDEfaultContext(serverName)
+    ContextRegistry.registerDEfaultContext(this)
 
     def msbuild = new MsBuild()
     msbuild.buildDatabase(serverName)
