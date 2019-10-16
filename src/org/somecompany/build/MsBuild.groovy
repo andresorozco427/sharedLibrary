0797ec002a15fce7ffebb58de5310ad07e8f5a16
andres orozco muñoz <andresorozco427@gmail.com>
2019-10-15 20:50:18 -0500

    Actualizacion jenkinsfile

--- src/org/somecompany/build/MsBuild.groovy
+++ src/org/somecompany/build/MsBuild.groovy
@@ -17,8 +17,8 @@ class MsBuild implements  Serializable{
    void buildDatabase(String serverName){
        IStepExecutor steps = ContextRegistry.getContext().getStepExecutor();
 
-       int returnStatus =  steps.bat("sqlcmd \" -S ${serverName} -E -i C:\\Users\\andres.orozco\\Desktop\\returnvalue.sql -o C:\\Users\\andres.orozco" +
-               "\\Desktop\\return.sql \"")
+       int returnStatus =  steps.bat("sqlcmd -S ${serverName} -E -i C:\\Users\\andres.orozco\\Desktop\\returnvalue.sql -o C:\\Users\\andres.orozco" +
+               "\\Desktop\\return.sql")
        if(returnStatus != 0){
            steps.error("Some error")
        }
